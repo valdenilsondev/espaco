7eaa9d74d7de5c9ad07a5d803579376efd258cf2
valdenilsondev <valdenilsondev@gmail.com>
2023-11-04 09:03:11 -0300

    Implementa novas mecâncias

---
+++ ATUALIZADO04_11_20232/teste/Assets/Scripts/finalJogo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finalJogo : MonoBehaviour {
+
+
+	public GameObject fimJogo;
+
+	// Use this for initialization
+	void Start () {
+
+		fimJogo.SetActive(false);
+
+	}
+
+	void Update()
+	{
+		
+	}
+
+
+	private void OnTriggerEnter(Collider other)
+	{
+
+		if (other.gameObject.tag == "Player")
+		{
+			StartCoroutine(MensagemFinal());
+		}
+	}
+	IEnumerator MensagemFinal()
+    {
+		yield return new WaitForSeconds(3f);
+		fimJogo.SetActive(true);
+	}
+}
